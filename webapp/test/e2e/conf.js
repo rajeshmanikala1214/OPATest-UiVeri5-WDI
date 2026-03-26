1c94b0555929c576e5a7ce5350d54e08730cfc4b
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-03-26 08:37:49 +0530

    changed conf.js file data related to uiveri5 test[Changes]

--- webapp/test/e2e/conf.js
+++ webapp/test/e2e/conf.js
@@ -1,22 +1,19 @@
 exports.config = {
     profile: 'integration',
     baseUrl: 'http://localhost:8080/index.html',
+    // REPORTERS ARE DEFINED HERE, NOT IN THE CLI
     reporters: [
         {
             name: 'junit',
-            reportDir: './target/report',
-            reportName: 'uiveri5-results'
-        },
-        {
-            name: 'json',
-            reportDir: './target/report',
+            reportDir: 'target/report',
             reportName: 'uiveri5-results'
         }
     ],
-    specs: ['*.spec.js'],
-    takeScreenshots: {
-        onExpectationFailure: true,
-        onExpectationSuccess: false,
-        onAction: false
-    }
+    capabilities: [{
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+        }
+    }],
+    specs: ['*.spec.js']
 };
