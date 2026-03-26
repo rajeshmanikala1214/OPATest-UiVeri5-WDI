75fb74aa7543dbff104c6cba88f3057703f4bdf1
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-03-26 12:48:33 +0530

    added testcases in opajourney

--- webapp/test/opa5/journey.test.js
+++ webapp/test/opa5/journey.test.js
@@ -6,17 +6,14 @@ sap.ui.define([
 
 	QUnit.module("Main Journey");
 
-	// Test Case 1: Navigation
 	opaTest("Should see the initial page", function (Given, When, Then) {
 		Given.iStartMyApp();
 		Then.onTheMainPage.iShouldSeeTheAppHeader();
 	});
 
-	// Test Case 2: Search/Filter
 	opaTest("Should filter the list on search", function (Given, When, Then) {
+		Given.iStartMyApp(); // 🔥 REQUIRED
 		When.onTheMainPage.iSearchFor("Laptop");
 		Then.onTheMainPage.iShouldSeeOnlyFilteredResults();
 	});
-    
-    // Add 8 more placeholders here for your demo...
 });
